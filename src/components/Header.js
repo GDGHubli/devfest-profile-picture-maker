b5d76b081b00c15708b5767bcde8e5da3eb40d42
Supriya Shashivasan <supi.shashivasan@gmail.com>
2022-09-23 03:32:42 +0530

    feat: Add common layout (headerand footer) (#5)
    * feat: Add responsive header and footer components
    
    * chore: Remove package-lock.json

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex flex-col justify-between w-full md:items-center md:flex-row">
+        <div className="w-full md:w-2/4">
+            <h1 className="text-xl font-bold leading-normal mb-6 md:text-5xl">Generate your DevFest 2022 theme profile picture</h1>
+            <p className="text-base md:text-lg">You can use this regardless of the country/city you are attending from.</p>
+        </div>
+        <img className="md:w-1/4 w-2/3" src="DevFest-Logo-2022-LightBackground.png" alt="DevFest Logo"/>
+    </div>
+  );
+}
+export default Header;
